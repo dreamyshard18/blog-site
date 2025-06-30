000713bda145b2b39fcaf56e326380176c60e851
dreamyshard18 <diyalesu@gmail.com>
2025-06-30 15:12:01 +0530

    Conflicts resolved

--- blog-site/src/App.jsx
+++ blog-site/src/App.jsx
@@ -3,7 +3,31 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'; // Import your CSS styles
 import Login from './components/Login';
 import Landing from './pages/Landing';
+import {useState} from 'react';
+const [newBlog,setnewBlog]=useState("");
 function App() {
+  const handlenewBlog=async ()=>{
+   const body={
+     content:newBlog,
+     title:"",
+     username:"",
+     createdAt:""
+   };
+   await fetch("http://localhost:8080", {
+     method: "POST",
+     headers: {
+       "Content-Type": "application/json"
+     },
+     body: JSON.stringify(body)
+   });
+   await handlenewBlog();
+  } 
+
+  const handlegetBlogs=async ()=>{
+    const response = await fetch("http://localhost:8080");
+    const data = await response.json();
+    setnewBlog(data.map(blog => blog.content));
+  }
   return (
     <div>
       <BrowserRouter>
